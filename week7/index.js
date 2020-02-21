47f56888fa35aabcfc057f0d2de76045d080d21b
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-02-21 13:28:45 -0500

    update hyperlink example

---
+++ week7/index.js
@@ -0,0 +1,11 @@
+indexPage = {
+	path_forms : function() {
+		window.location.href='https://yuhsuan-huang-tw.000webhostapp.com/forms/index.html'
+	},
+	path_assign2 : function() {
+		window.location.href='https://yuhsuan-huang-tw.000webhostapp.com/assign2/index.html'
+	},
+	path_weather : function() {
+		window.location.href='https://yuhsuan-huang-tw.000webhostapp.com/weather/index.html'
+	}
+}
